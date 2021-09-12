843a3ff04256b58b397345c9fa8e3f1e2a3d543a
Kirill Kovalev <kirilkovalev@yandex.ru>
2021-09-12 20:53:39 +0300

    * | config | fixed db connection * | build | fixed module name * | docker-compose | updated for single db

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-mvn clean package
 
 moduleName="suai"
 
 exportDirectory="target/docker/$moduleName"
-buildBranch=$(git rev-parse --abbrev-ref HEAD)
+buildBranch=$(git rev-parse --abbrev-ref HEAD | tr -d '-'| tr -d '/')
 buildNumber=$(git rev-list HEAD | wc -l | tr -d ' ')
 buildVersion="$buildBranch.$buildNumber"
 
+mvn -Dmaven.test.skip=true clean package
 echo Y | docker image prune -a
-docker build -t schedapp/suai:latest -t schedapp/suai:$buildVersion .
+docker build -t schedapp/$moduleName:latest -t schedapp/$moduleName:$buildVersion .
 mkdir -p ./$exportDirectory
 echo Y | docker image prune
-docker save -o ./$exportDirectory/image.tar schedapp/suai
+docker save -o ./$exportDirectory/image.tar schedapp/$moduleName
 cp ./docker-compose.yml ./$exportDirectory/docker-compose.yml
