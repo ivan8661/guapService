46d53e8cbeeba10665b2ad356dd9e0ed6f15dc31
Kirill Kovalev <kirilkovalev@yandex.ru>
2021-09-26 17:15:32 +0300

    * | build.sh | updated script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 moduleName="suai"
+projectName="schedapp"
+dockerRegistry="docker.poltorakov.ru"
+
+exportDirectory="target/docker/$moduleName"
+imageName="$dockerRegistry/$projectName/$moduleName"
 
 exportDirectory="target/docker/$moduleName"
 buildBranch=$(git rev-parse --abbrev-ref HEAD | tr -d '-'| tr -d '/')
@@ -8,9 +13,12 @@ buildNumber=$(git rev-list HEAD | wc -l | tr -d ' ')
 buildVersion="$buildBranch.$buildNumber"
 
 mvn -Dmaven.test.skip=true clean package
+
 echo Y | docker image prune -a
-docker build -t schedapp/$moduleName:latest -t schedapp/$moduleName:$buildVersion .
+docker build -t $imageName:latest \
+             -t $imageName:$buildVersion .
 mkdir -p ./$exportDirectory
+
 echo Y | docker image prune
-docker save -o ./$exportDirectory/image.tar schedapp/$moduleName
+docker save -o ./$exportDirectory/image.tar $dockerRegistry/$projectName/$moduleName
 cp ./docker-compose.yml ./$exportDirectory/docker-compose.yml
